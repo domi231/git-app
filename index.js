dd9cdeb97a8ce256c1c86645e7a31a5c9f198f39
domi_231 <pizzadequeso524@gmail.com>
2023-01-01 20:32:48 +0100

    repositorio bomba

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import http from 'http'
 
 
 const servidor = http.createServer((req,res) => {
-    res.send("Hola amigo")
+    res.writeHead(200,{'Content-Type' : 'application/json'})
+    res.send()
 })
 
-servidor.listen(3000)
+
+
+servidor.listen(3000,()=> {
+    console.log("Servidor iniciado")
+})
